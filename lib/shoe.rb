62a9d0b9cb8242cd15a04e0b87cd3e921af73dbe
Jason Fournier <jmf4nier@hotmail.com>
2019-05-15 18:37:30 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,11 +2,15 @@ require 'pry'
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-  @@BRANDS = [ ]
+  BRANDS = [ ]
   def initialize(brand)
     @brand = brand
-    @@BRANDS << brand
+    BRANDS << brand
   end
+  def brands
+    BRANDS = brands
+  end
+  
   def brand=(brand)
     @brand = brand
   end
